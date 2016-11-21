82bc34d864f68cacb9974f4d3d9daa3f1b604bf3
kangxd <kxdsg@126.com>
2016-11-21 14:46:13 +0800

    不同环境读取不同配置

--- src/main/java/com/web/api/controller/IndexController.java
+++ src/main/java/com/web/api/controller/IndexController.java
@@ -1,5 +1,10 @@
 package com.web.api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +17,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
+    public static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private Environment env;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(ModelMap map){
         map.put("msg","Welcome to study spring boot!");
+        logger.info("filepath: " + env.getProperty("file.path")); //测试不同环境读取不同配置属性
         return "index"; //html模板名称,对应/templates下的index.html
     }
 
